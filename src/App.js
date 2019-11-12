c2a2ba365f72d5ca9f6f9f7b509fdee0b9e644c3
Sethulakshmi <sethulakshmi.pm@razorthink.com>
2019-11-12 16:39:26 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Welcome to Embibe</h2>
+      <form className="student-login-wrapper">
+
+      </form>
+    </div>
+  );
+}
+
+export default App;
